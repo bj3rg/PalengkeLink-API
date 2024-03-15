5080888350801a0fd7709c25dbea71f3973ee5e0
Angelo Burog <angelokburog30@gmail.com>
2024-03-15 13:56:39 +0800

    add vouchers model

---
+++ models/Voucher.js
@@ -0,0 +1,46 @@
+const Sequelize = require("sequelize");
+const sequelizeConnect = require("../connection/database");
+
+const Vouchers = sequelizeConnect.define(
+  "Vouchers",
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    voucher_code: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    validity_date: {
+      type: Sequelize.DATEONLY,
+      allowNull: false,
+    },
+    percent_off: {
+      type: Sequelize.DECIMAL(10, 5),
+      allowNull: false,
+    },
+    is_available: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    is_single_use: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    tableName: "vouchers",
+    timestamps: true,
+  }
+);
+
+module.exports = Vouchers;
