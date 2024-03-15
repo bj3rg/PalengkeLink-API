c4b0ec34d385cf5831b0e79e73ae29ef99491fb4
koiseee <detorresmhel02@gmail.com>
2024-03-15 20:54:43 +0800

    model

---
+++ models/Cart.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+const sequelizeConnect = require("../connection/database");
+
+const Cart = sequelizeConnect.define(
+  "cart",
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    user_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    total_price: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    tableName: "cart",
+    timestamps: true,
+  }
+);
+
+module.exports = Cart;
---
+++ models/Transactions.js
@@ -0,0 +1,44 @@
+const Sequelize = require("sequelize");
+const sequelizeConnect = require("../connection/database");
+
+const Transactions = sequelizeConnect.define(
+  "transactions",
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    user_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    item_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    voucher_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    payment_method: {
+      type: Sequelize.ENUM("Gcash", "CashOnDelivery", "PalengkeCoins"),
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "transactions",
+    timestamps: true,
+  }
+);
+
+module.exports = Transactions;
