78c76a0290fab53a91ab7d5477668c5c06208d52
Angelo Burog <angelokburog30@gmail.com>
2024-03-15 14:49:25 +0800

    add rating model

---
+++ models/Ratings.js
@@ -0,0 +1,53 @@
+const Sequelize = require("sequelize");
+const sequelizeConnect = require("../connection/database");
+const User = require("../models/User");
+const Product = require("../models/Product");
+const Ratings = sequelizeConnect.define(
+  "Ratings",
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    user_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    product_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    rating: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "ratings",
+    timestamps: true,
+  }
+);
+
+Ratings.belongsTo(User, {
+  foreignKey: "user_id",
+  targetKey: "id",
+  as: "user",
+});
+
+Ratings.belongsTo(Product, {
+  foreignKey: "product_id",
+  targetKey: "id",
+  as: "product",
+});
+
+module.exports = Ratings;
