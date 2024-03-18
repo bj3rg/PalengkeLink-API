46088356e855f13af7fa5f1c582a8a5e5eb10ef4
Kenneth <kinetzki2k20@gmail.com>
2024-03-18 14:15:36 +0800

    feat: added routes for usrs

--- controllers/User.js
+++ controllers/User.js
@@ -60,9 +60,91 @@ exports.updateUser = (req, res, next) => {
     phone_number,
     email_address,
   } = req.body;
+  Users.findOne({
+    where: {
+      id: userId,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        return res
+          .status(400)
+          .json({ success: false, message: "User id does not exist" });
+      } else {
+        data.first_name = first_name;
+        data.last_name = last_name;
+        data.middle_name = middle_name;
+        data.address = address;
+        data.phone_number = phone_number;
+        data.email_address = email_address;
+        return data.save().then(() => {
+          return res
+            .status(200)
+            .json({ success: true, message: "User info updated" });
+        });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
+
 //update user status
-//delete user
+exports.updateStatus = (req, res, next) => {
+  const { userId, stat } = req.params;
+  Users.findOne({
+    where: {
+      id: userId,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        return res
+          .status(400)
+          .json({ success: false, message: "User id does not exist" });
+      } else if (data.status === stat) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Status already set to " + stat });
+      } else {
+        data.status = stat;
+        return data.save().then(() => {
+          return res
+            .status(200)
+            .json({ success: true, message: "User info updated" });
+        });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+  const {userId} = req.params;
+  Users.findOne({
+    where: {
+      id: userId,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        return res
+          .status(400)
+          .json({ success: false, message: "User id does not exist" });
+      } else {
+        return data.destroy().then(() => {
+          return res
+            .status(200)
+            .json({ success: true, message: "User deleted permanently" });
+        });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
 //get user by id
 //get all users status true
 //get
--- routes/user/user.js
+++ routes/user/user.js
@@ -1,10 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, param } = require("express-validator");
+const validation = require("../../middlewares/routeValidation");
+const { createUser, updateUser, updateStatus, deleteUser } = require("../../controllers/User");
 
-const { createUser, updateUser } = require("../../controllers/User");
+router.post(
+  "/create",
+  [
+    body("first_name").notEmpty(),
+    body("last_name").notEmpty(),
+    body("middle_name").notEmpty(),
+    body("address").notEmpty(),
+    body("phone_number").notEmpty(),
+    body("email_address").notEmpty(),
+    body("gender").notEmpty(),
+    body("birth_date").notEmpty(),
+    body("fcm_token").notEmpty(),
+    body("password").notEmpty(),
+    body("status").notEmpty(),
+  ],
+  validation,
+  createUser
+);
 
-router.post("/create", createUser);
-router.put("/:userId/update", [], updateUser);
+router.put("/:userId/update", [
+    body("first_name").notEmpty(),
+    body("last_name").notEmpty(),
+    body("middle_name").notEmpty(),
+    body("address").notEmpty(),
+    body("phone_number").notEmpty(),
+    body("email_address").notEmpty(),
+    param("userId").notEmpty()
+], validation, updateUser);
 
+router.put("/:userId/:stat/update-status", [], updateStatus);
+router.delete("/:userId/hard-delete", [], deleteUser);
 
 module.exports = router;
