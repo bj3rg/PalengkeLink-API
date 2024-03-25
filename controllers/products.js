b0c4c2fb4629624547d4f90c3fd3cc056091adb3
DevKiane <descbls@gmail.com>
2024-03-25 10:00:41 +0800

    product update

--- controllers/products.js
+++ controllers/products.js
@@ -23,32 +23,7 @@ exports.createProduct = (req, res, next) => {
 
   const productImage = req.files.productImage[0];
 
-  Category.findOne({
-    where: {
-      id: categoryId,
-    },
-  })
-    .then((category) => {
-      if (!category) {
-        return res.status(400).json({
-          success: false,
-          message: "Category not found",
-        });
-      }
-      return Products.findOne({
-        where: {
-          product_name: productName,
-        },
-      });
-    })
-    .then((existingProduct) => {
-      if (existingProduct) {
-        return res.status(400).json({
-          success: false,
-          message: "Product name already exists!",
-        });
-      } else {
-        return Products.create({
+  Products.create({
           category_id: categoryId,
           product_name: productName,
           price: price,
@@ -57,9 +32,7 @@ exports.createProduct = (req, res, next) => {
           product_image: productImage.filename || null,
           stocks: stocks,
           purchase_count: purchaseCount,
-        });
-      }
-    })
+        })
     .then(() => {
       return res.status(200).json({
         success: true,
@@ -75,54 +48,49 @@ exports.createProduct = (req, res, next) => {
     });
 };
 
-exports.updateProduct = (req, res, next) => {
-  const productId = req.params.productId;
-  const {
-    categoryId,
-    productName,
-    price,
-    description,
-    ratingsId,
-    stocks,
-    purchaseCount,
-    status,
-  } = req.body.data || {}; // Destructure req.body.data with default value as empty object
+// exports.updateProduct = (req, res, next) => {
+//   const {productId} = req.params.productId;
+//   const {
+//     categoryId,
+//     productName,
+//     price,
+//     description,
+//     ratingsId,
+//     stocks,
+//     purchaseCount,
+//     status,
+//   } = req.body.data || {}; 
+//   Products.findOne({ where: { id: productId } })
+//     .then((product) => {
+//       if (!product) {
+//         errorHandler("Product not found!", 404);
+//       }
 
-  // Logic to update product details
-  Products.findOne({ where: { id: productId } })
-    .then((product) => {
-      if (!product) {
-        errorHandler("Product not found!", 404);
-      }
+//       if (req.files && req.files.productImage) {
+//         const productImage = req.files.productImage[0];
+//         deleteFile(product.product_image, "product-upload"); 
+//         product.product_image = productImage.filename || null;
+//       }
 
-      // If product image exists in request, delete the old image and update the image filename
-      if (req.files && req.files.productImage) {
-        const productImage = req.files.productImage[0];
-        deleteFile(product.product_image, "product-upload"); // Delete old image
-        product.product_image = productImage.filename || null; // Update image filename
-      }
+//       if (categoryId) product.category_id = categoryId;
+//       if (productName) product.product_name = productName;
+//       if (price) product.price = price;
+//       if (description) product.description = description;
+//       if (ratingsId) product.ratingsId = ratingsId;
+//       if (stocks) product.stocks = stocks;
+//       if (purchaseCount) product.purchase_count = purchaseCount;
+//       if (status) product.status = status;
 
-      // Update other product properties if provided
-      if (categoryId) product.category_id = categoryId;
-      if (productName) product.product_name = productName;
-      if (price) product.price = price;
-      if (description) product.description = description;
-      if (ratingsId) product.ratingsId = ratingsId;
-      if (stocks) product.stocks = stocks;
-      if (purchaseCount) product.purchase_count = purchaseCount;
-      if (status) product.status = status;
-
-      // Save the updated product
-      return product.save();
-    })
-    .then((updatedProduct) => {
-      return res.status(200).json({
-        success: true,
-        message: "Product updated successfully",
-        data: updatedProduct,
-      });
-    })
-    .catch((err) => {
-      next(err);
-    });
-};
+//       return product.save();
+//     })
+//     .then((updatedProduct) => {
+//       return res.status(200).json({
+//         success: true,
+//         message: "Product updated successfully",
+//         data: updatedProduct,
+//       });
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// };
--- routes/products/products.js
+++ routes/products/products.js
@@ -13,14 +13,38 @@ const {
   // findProductsByCategory,
   // findActiveProductsByCategory
 } = require("../../controllers/products");
+const errorHandler = require("../../util/errorHandler");
+const { updateCategory } = require("../../controllers/category");
 
 router.post(
   "/create",
   jsonParser,
   [
-    body("data.categoryId").notEmpty(),
-    body("data.productName").notEmpty(),
-    body("data.price").notEmpty,
+    body("data.categoryId").notEmpty()
+    .custom((value, { req }) => {
+      return Category.findOne({
+        where: {
+          id: value,
+        },
+      }).then((category) => {
+        if (!category) {
+          return Promise.reject("Category ID does not exists!");
+        }
+      });
+    }),
+    body("data.productName").notEmpty()
+    .custom((value, { req }) => {
+      return Products.findOne({
+        where: {
+          product_name: value,
+        },
+      }).then((product) => {
+        if (product) {
+          return Promise.reject("Product already exists!");
+        }
+      });
+    }),
+    body("data.price").notEmpty(),
     body("data.description").notEmpty(),
     body("data.ratingsId").optional(),
     body("data.stocks").notEmpty(),
@@ -28,6 +52,44 @@ router.post(
   ],
   validation,
   createProduct
+);
+
+router.post(
+  "/:productId/update",
+  jsonParser,
+  [
+    body("data.categoryId").optional()
+    .custom((value, { req }) => {
+      return Category.findOne({
+        where: {
+          id: value,
+        },
+      }).then((category) => {
+        if (!category) {
+          return Promise.reject("Category ID does not exists!");
+        }
+      });
+    }),
+    body("data.productName").optional()
+    .custom((value, { req }) => {
+      return Products.findOne({
+        where: {
+          product_name: value,
+        },
+      }).then((product) => {
+        if (product) {
+          return Promise.reject("Product already exists!");
+        }
+      });
+    }),
+    body("data.price").optional(),
+    body("data.description").optional(),
+    body("data.ratingsId").optional(),
+    body("data.stocks").optional(),
+    body("data.purchaseCount").optional(),
+  ],
+  validation,
+  // updateProduct
 );
 
 module.exports = router;
